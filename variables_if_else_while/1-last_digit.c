3faa7a20f7acfac1aed240fae75e84ea5d1e59fc
Akatsuki667 <qextzy@gmail.com>
2024-10-22 10:05:20 +0200

    change C

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ last_digit = n % 10;
 printf("Last digit of %d is ", n);
 if (last_digit > 5)
 {
-printf(" %d is greater than 5\n", last_digit);
+printf("%d is greater than 5\n", last_digit);
 }
 else if (last_digit == 0)
 {
