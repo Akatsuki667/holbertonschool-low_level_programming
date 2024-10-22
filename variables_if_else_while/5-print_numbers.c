57449b8ca2a3a596c101395c960a5e16ad7e29a7
Akatsuki667 <qextzy@gmail.com>
2024-10-22 14:05:45 +0200

    add README.md, add file, add C

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - description
+ * Return : 0 for success
+ */
+
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
