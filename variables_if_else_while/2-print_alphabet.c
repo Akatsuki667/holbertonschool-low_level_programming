d026f5017176a1c6e2055d3746dcbbcec263192e
Akatsuki667 <qextzy@gmail.com>
2024-10-22 10:36:30 +0200

    add README.md, add file, add C

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - description
+  * Return: 0 for success
+  */
+
+int main(void)
+{
+char aplhabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet)
+}
+return (0);
+}
