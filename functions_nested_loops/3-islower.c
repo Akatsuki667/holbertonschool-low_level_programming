a9b25dd6daf969d9fa81f69a469994acaad71402
Akatsuki667 <qextzy@gmail.com>
2024-10-24 18:57:13 +0200

    Betty style go to the hell

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,11 @@
 #include "main.h"
-/*
- * main - description
+/**
  * _islower - description
  * Return: 0
  */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
-/* Condition vérifie si la valeur de c est comprise entre 97 et 122 (codes ASCII minuscules) */
 {
 return (1);
 /* lowercase character */
