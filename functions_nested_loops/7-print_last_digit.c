ef38f783af8605f5f5ae3604a75f21d417204798
Akatsuki667 <qextzy@gmail.com>
2024-10-25 10:49:26 +0200

    add README.md, add file, add C

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
- * print_last_digit - description
- * @i: description
- * Return: description
-  */
+ * print_last_digit - Calcule et affiche le dernier chiffre d'un entier
+ * @i: L'entier dont on souhaite obtenir le dernier chiffre
+ * Return: Le dernier chiffre de l'entier (toujours positif)
+ */
 int print_last_digit(int i)
 {
 int last_digit = i % 10;
+/* Calcule le dernier chiffre de i */
 if (last_digit < 0)
 /* Assure que last_digit est positif */
 {
 last_digit = -last_digit;
 }
-_putchar(last_digit);
+_putchar(last_digit + '0');
+/* Affiche le dernier chiffre comme un caractère */
 return (last_digit);
+/* Retourne le dernier chiffre en tant qu'entier */
 }
