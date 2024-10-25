939f80ad4542c52147530979d5137a96b3fe6f00
Akatsuki667 <qextzy@gmail.com>
2024-10-25 10:18:33 +0200

    add README.md, add file, add C

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - description
+ * @i: description
+ * Return: description
+ */
+int _abs(int i)
+{
+if (i < 0)
+{
+int abs = i * -1;
+return (abs);
+}
+else
+{
+return (i);
+}
+}
