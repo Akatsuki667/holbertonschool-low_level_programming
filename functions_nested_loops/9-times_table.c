e20c240f33a47aa1df35bb777cd89341f699631d
Akatsuki667 <qextzy@gmail.com>
2024-10-27 16:14:30 +0100

    add README.md, add file, add C

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,19 +7,33 @@
 void times_table(void)
 {
 int x, y, z, u, d;
+/**
+ * x : représente les multiplicateurs
+ * y : repésentente les multiplicandes
+ * z : Stockage résultat x*y
+ * u / d : extraire chiffre dizaines / unités lorsque z>9
+ */
 for (x = 0; x <= 9; x++)
+/* itère sur chaque ligne table de 9 */
 {
 for (y = 0; y <= 9; y++)
+/* itère sur chaque colonne */
 {
 z = x * y;
 if (z > 9)
 {
 u = z % 10;
+/* donne le chiffre des unités */
 d = (z - u) / 10;
+/* donne le chiffre des dizaines */
 _putchar(44);
+/* imprime une virgule */
 _putchar(32);
+/* imprime un espace */
 _putchar(d + '0');
-_putchar(u + '0');
+/* impression des dizaines (décalé par '0' pour convertir en ASCII) */
+_putchar(u + '0')
+/* impression unités */
 }
 else
 {
