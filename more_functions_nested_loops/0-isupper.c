655b2c073f95bba6c4aee340bd55a5fb602d0af1
Akatsuki667 <qextzy@gmail.com>
2024-10-28 10:07:30 +0100

    add README.md, add file, add C

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - description
+ * @c: description
+ * Return: 1 for uppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+/* condition vérifiant que c une majsucule */
+{
+return (1);
+/* si la condition est vrai elle retourne 1 */
+}
+else
+{
+return (0);
+/* sinon elle retourne 0 */
+}
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+Do the task of the project "C-More functions, more nested loops"
+
+TASKS :
+
+0-isupper.c : Write a function that checks for uppercase character.
+-Returns 1 if c is uppercase
+-Returns 0 otherwise
+
