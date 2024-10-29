0201453d2da257fc852111135bd58bf223e4f4d2
Akatsuki667 <qextzy@gmail.com>
2024-10-29 11:42:49 +0100

    add README.md, add file, add C

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - description
+ * @a: description
+ * @b: description
+ */
+void swap_int(int *a, int *b)
+{
+int x = *a;
+/* variable temporaire = sauvegarde valeur intiale avant modification */
+*a = *b;
+/* affecte valeur pointée par b à l'meplacemnt pointée par a */
+*b = x;
+}
