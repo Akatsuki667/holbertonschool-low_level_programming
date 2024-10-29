8fb51ee918c80c49b7a9160105d6306276d54090
Akatsuki667 <qextzy@gmail.com>
2024-10-29 15:51:51 +0100

    add README.md, add file, add C

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - description
+ * _putchar - description
+ * @s: description
+ */
+void print_rev(char *s)
+{
+int i;
+/* parcourir la chaîne de la fin vers le début */
+int reverse = 0;
+/* compte nb de caractère */
+while (s[reverse] != '\0')
+/* boucle itère jusqu'au carcatère NULL */
+{
+reverse++;
+}
+for (i = reverse -1; i >= 0; i--)
+/* affichage reverse */
+{
+_putchar(s[i]);
+/* affcihe le carcatère de l'indice [i] */
+}
+_putchar('\n');
+}
