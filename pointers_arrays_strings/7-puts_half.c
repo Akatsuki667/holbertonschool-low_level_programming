80285ff183f77d2d1924596135b6837333f986cf
Akatsuki667 <qextzy@gmail.com>
2024-10-30 15:38:35 +0100

    change code

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 void puts_half(char *str)
 {
-char *ptr = str;
-/* déclaration pointeur adresse mémoire str */
-char *str_1 = str;
-/* permet manipulation de la chaîne sans perdre valeur initiale */
-while (*ptr != '\0')
-ptr++;
-/* Déplacement pointeur jusqu'à le caractère NULL */
-str_1 = str + ((ptr + 1 - str) / 2);
-/* valeur pointeur équivalent calcul de la longuer désirer str */
-while (*str != '\0')
-{
-if (str < str_1)
-str++;
-else
-{
-_putchar(*str);
-str++;
-}
-}
+         char *right_bound = str, *left_bound = str;
+
+         while (*right_bound)
+                 right_bound++;
+         left_bound = str + ((right_bound + 1 - str) / 2);
+         while (*str)
+         {
+         if (str >= left_bound)
+         {
+                 _putchar(*str);
+                 str++;
+         }
+         else
+                 str++;
+         }
+         _putchar(10);
 }
