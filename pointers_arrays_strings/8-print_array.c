c59cdab35b8a19d178cfea73b5834a78913d49c1
Akatsuki667 <qextzy@gmail.com>
2024-10-30 16:48:39 +0100

    add README.md, add file, add C

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - description
+ * @a : description
+ * @n : description
+ */
+void print_array(int *a, int n)
+{
+int i;
+/* index pour parcourir éléments tableau a */
+for (i = 0; i < n; i++)
+/* parcours premiers éléments tableau */
+{
+if (i == n - 1)
+printf("%d", a[i]);
+/* condition vérifiant si a[i] est le dernier élément */
+/* permettant d'afficher le dernier chiffre sans virgule */
+else
+printf("%d, ", a[i]);
+}
+}
