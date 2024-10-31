2a45bbf85c06a42ed15d5153552c85b8061dc2bd
Akatsuki667 <qextzy@gmail.com>
2024-10-31 16:10:29 +0100

    add README.md, add file, add C

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _atoi - description
+ * @s: description
+ * Return: nb
+ */
+int _atoi(char *s)
+{
+int sign = 1;
+/* déclaration varibale récuperation sign */
+/* 1 : pour récupérer nb positif */
+int num = 0;
+/* variable récupération de nb */
+int i;
+/* index itération *s */
+int j;
+int check = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= '0' && s[i] <= '9')
+check = 1;
+else if (check == 1)
+break;
+if (s[i] == '-')
+sign = sign * (-1);
+}
+check = 0;
+for (j = 0; s[j] != '\0'; j++)
+{
+if (s[j] >= '0' && s[j] <= '9')
+{
+check = 1;
+num = num * 10 + s[j] - '0';
+}
+else if (check == 1)
+break;
+}
+return (num *sign);
+/* retourner le nb */
+}
