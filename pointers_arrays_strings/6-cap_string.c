e1fdea40adaefb7d6f6fda258c16c309c59c4375
Akatsuki667 <qextzy@gmail.com>
2024-11-03 20:10:30 +0100

    change code

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * cap_string - Capitalizes all words of a string
  * @str: The string to capitalize
@@ -7,20 +8,34 @@
  */
 char *cap_string(char *str)
 {
-int i = 0;
-/* index parcourir caractère de la chaîne */
+int i = 0; /* Index for traversing the string */
+int j;
+/* Define the word separators */
 char separators[] = " \t\n,;.!?\"(){}";
-/* array contenant caractère séparateurs mots */
+/* Capitalize the first character if it's a lowercase letter */
 if (str[i] >= 'a' && str[i] <= 'z')
-/* condition vérifiant si str[i] est une minuscule */
-str[i] -= 32;
+{
+str[i] -= 32; /* Convert to uppercase */
+}
+/* Iterate through the string */
 while (str[i] != '\0')
-if (separators[str[i]])
-/* condition vérifiant si str[i] est un separators */
+{
+/* Check if the current character is a separator */
+for (j = 0; separators[j] != '\0'; j++)
+{
+if (str[i] == separators[j]) /* Found a separator */
+{
+/* Move to the next character */
 i++;
+/* If the next character is a lowercase letter, capitalize it */
 if (str[i] >= 'a' && str[i] <= 'z')
-str[i] -= 32;
-else
-i++;
-return (str);
+{
+str[i] -= 32; /* Convert to uppercase */
+}
+break; /* Exit the inner loop since a separator was found */
+}
+}
+i++; /* Move to the next character */
+}
+return (str); /* Return the pointer to the modified string */
 }
