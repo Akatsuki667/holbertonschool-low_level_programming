63d431067720774e3fa69359c853fbacadf1f26a
Akatsuki667 <qextzy@gmail.com>
2024-11-05 18:26:01 +0100

    add README.md, add file, add C

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - focntion affiche chaîne de caractère
+ * _putchar - affiche caractère par caractère
+ * @s: chaîne de caractère à affciher
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+/* vérification si caractère actuel n'est pas le caractère NULL*/
+_putchar(*s);
+/* afichage carcatère actuel */
+s++;
+/* incrémente le pointeur vers le caractère suivant */
+_puts_recursion(s);
+/* appel récursif de la fonction */
+}
+else
+_putchar('\n');
+/* si fin de la chaîne affiche retour à la ligne */
+}
---
+++ recursion/README.md
@@ -0,0 +1,5 @@
+Do the tasks of project "C-recursion"
+
+TASKS :
+
+0-puts_recursion.c : Write a function that prints a string, followed by a new line.
