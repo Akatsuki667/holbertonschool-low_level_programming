26a64c40e99097146fbad17d8d374eacf1cd3d90
Akatsuki667 <qextzy@gmail.com>
2024-11-06 15:50:53 +0100

    change code

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - afficher le nb d'argument
+ * @argc: nd d'arguments ligne de commande
+ * @argv: tableau arguments ligne de commande
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 printf("%d\n", argc);
