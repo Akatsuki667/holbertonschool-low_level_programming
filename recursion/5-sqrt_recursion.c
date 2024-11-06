217e18577d40bb480f1b3fcc75f6d04ea92f47a6
Akatsuki667 <qextzy@gmail.com>
2024-11-06 11:38:38 +0100

    add README.md, add file, add C

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * sqrt_clc - fonction auxiliare calcul racine carré
+ * @n: valeur pour lequel le calcul est implémenter
+ * @r: candidat racine carré, incrémenter à chaque appel
+ * Return: retourne racine carré ou -1
+ */
+int sqrt_clc(int n, int r)
+/* focntion auxiliare pour calculer la racine carré de n */
+/* n = nb dont on cherche la racine carré */
+/* r = une tentative pour racine carré qui augmente à chaque appel */
+{
+if ((r * r) == n) /* condition de sortie */
+/* si r racine carré de n */
+return (r);
+/* retourne valeur souhaité */
+if ((r * r) > n) /* condition de sortie */
+/* si r supérieur à n */
+return (-1);
+/* si r trop grand, son carré dépasse n -> n pas de racine carré entière */
+/* return (-1) utilisé pour signaler une erreur ou valeur non défini */
+return (sqrt_clc(n, r + 1));
+/* si les une des 2 conditions pas rempli -> appel récursif avec r+1*/
+/* continue jusqu'a ce que une des deux condition rempli */
+}
+/**
+ * _sqrt_recursion - calcule racine carré entière d'un nb
+ * @n: nb dont on cherche la racine carré
+ * Return: retourne racine carré ou -1
+ */
+int _sqrt_recursion(int n) /* fonction principal pour calcul racine carré */
+{
+if (n < 0) /* condition de sortie */
+/* vérification n est négatif */
+return (-1);
+/* return (-1) utilisé pour signaler une erreur ou valeur non défini */
+return (sqrt_clc(n, 0));
+/* appel sqrt_clc, r = 0 signifie départ de l'incrémentation */
+}
--- recursion/README.md
+++ recursion/README.md
@@ -14,3 +14,6 @@ TASKS :
 
 4-pow_recursion.c : Write a function that returns the value of x raised to the power of y.
 - If y is lower than 0, the function should return -1
+
+5-sqrt_recursion.c : Write a function that returns the natural square root of a number.
+- If n does not have a natural square root, the function should return -1
