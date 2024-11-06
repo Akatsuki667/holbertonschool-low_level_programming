21cfbeda11aba318a8d7cff822eb4e5da66e940b
Akatsuki667 <qextzy@gmail.com>
2024-11-06 09:37:31 +0100

    add README.md, add file, add C

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - renvoyer longueur string
+ * @s: string que l'on doit calculer
+ * Return: retourne la longueur de la string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+/* vérification caracrtère actuel n'est pas NULL */
+return (0);
+/* Si caractère NULL, retourne 0*/
+else
+return (1) + _strlen_recursion(s + 1);
+/* appel récursif fonction */
+/* return 1 = ajoute 1 à la fonction */
+/* s+1 = déplace le pointeur */
+}
