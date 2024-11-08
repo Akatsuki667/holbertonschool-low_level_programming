d7d75545e3a8cb8c9639aed151b1ba27437a8a58
Akatsuki667 <qextzy@gmail.com>
2024-11-08 15:08:16 +0100

    add README.md, add file, add C

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplier les arguments
+ * @argc: nd d'arguments ligne de commande
+ * @argv: tableau arguments ligne de commande
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+/* Vérifie si le nombre d'arguments est correct */
+{
+printf("Error\n");
+/* Affiche "Error" si le nombre d'arguments est incorrect */
+return (1);
+/* Retourne 1 pour indiquer une erreur */
+}
+/* Convertit les arguments en entiers */
+int num1 = atoi(argv[1]);
+/* Convertit le premier argument (argv[1]) en entier */
+int num2 = atoi(argv[2]);
+/* Convertit le deuxième argument (argv[2]) en entier */
 
+/* Effectue la multiplication et affiche le résultat */
+int result = num1 * num2; /* Multiplie les deux nombres */
+printf("%d\n", result); /* Affiche le résultat suivi d'un saut de ligne */
+return (0);
+/* Retourne 0 pour indiquer que le programme s'est exécuté avec succès */
+}
