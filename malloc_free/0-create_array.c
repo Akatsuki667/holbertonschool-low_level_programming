dbf96af62121c22e02235916b8eecd90bf0c9a46
Akatsuki667 <qextzy@gmail.com>
2024-11-11 12:50:03 +0100

    add README.md, add file, add C

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * create_array - création tableau et alocation mémoire tableau
+ * @size: taille du tableau
+ * @c: nb de caractère
+ * Return: le tableau
+ */
+char *create_array(unsigned int size, char c)
+{
+char *array;
+/* pointeur stockera adresse mémoire bloc alloué */
+unsigned int i;
+/* index boucle for */
+if (size == 0)
+return (NULL);
+/* vérification size, évite d'allouer de la mémoire inutilement */
+array = malloc(size * sizeof(char));
+/* allocation mémoire de taille size et de type char*/
+if (array == NULL)
+return (NULL);
+/* vérification allocation mémoire n'a pas échoué*/
+for (i = 0; i < size; i++)
+/* parcourir chaque élément de array */
+array[i] = c;
+/* remplir place alloué avec c*/
+return (array);
+/* retourne pointeur tableau alloué et rempli */
+}
