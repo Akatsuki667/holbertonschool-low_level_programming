0a527df8fa4cdda2fffb042cf3bd59cada689e09
Akatsuki667 <qextzy@gmail.com>
2024-11-12 10:51:10 +0100

    add README.md, add file, add C

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,7 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * free_grid - libération allocation mémoire
+ * @grid: tableau à libérer
+ * @height: lignes à libérer
+ */
 void free_grid(int **grid, int height)
 {
-
+int i = 0; /* index parcourir lignes */
+while (i < height)
+{
+free(grid[i]); /* libération élément dans la ligne */
+i++; /* incrémentation adresse mémoire suivante */
+}
+free(grid); /* libération mémoire tableau */
 }
