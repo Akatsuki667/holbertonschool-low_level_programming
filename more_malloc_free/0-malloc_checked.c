7e0d336af249f94b1dd9d1ffa56fa12e5464b89d
Akatsuki667 <qextzy@gmail.com>
2024-11-12 15:18:37 +0100

    add README.md, add file, add C

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocation mémoire, vérification de cette allocation
+ * @b: quantité de mémoire à allouer
+ * Return: pointeur contenant adresse mémoire
+ */
+void *malloc_checked(unsigned int b)
+{
+char *array;
+/* pointeur mémoire allouer */
+array = malloc(b * sizeof(char));
+/* allocation bloc mémoire */
+if (array == NULL)
+/* vérification échec malloc */
+exit(98);
+/* fin d'exécution */
+/* code de sortie -> 98 */
+return (array);
+/*  retour pointeur adresse mémoire */
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,7 @@
+Do the tasks of project "C- More malloc, free"
+
+TASKS :
+
+0-malloc_checked.c : Write a function that allocates memory using malloc.
+- Returns a pointer to the allocated memory
+- if malloc fails, the malloc_checked function should cause normal process termination with a status value of 98
