534fc55c7380fff67206ab13b24335710d2bf9f6
Akatsuki667 <qextzy@gmail.com>
2024-11-12 18:21:30 +0100

    add README.md, add file, add C

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - création tableau de int
+ * @min: plus petit int
+ * @max: plus grand int
+ * Return: poiteur vers le tableau ou NULL
+ */
+int *array_range(int min, int max)
+{
+int *array;
+/* pointeur stocker adresse tableau allouer dynamiquement */
+int i;
+/* index boucle remplir array */
+int calcul = max - min + 1;
+/* calcul nb total éléments */
+if (min > max) /* vérification paramètres valide */
+return (NULL); /* paramètres non valide retour fonction NULL */
+array = malloc(calcul * sizeof(int));
+/* allocation mémoire */
+if (array == NULL)
+/* vérification malloc */
+return (NULL);
+/* échec malloc retour fonction NULL */
+for (i = 0; i < calcul; i++)
+/* initialisation/vérification/MAJ index i à chaque itération */
+array[i] = min + 1;
+/* affectation valeur à chaque position i dans array */
+/* array[i] = cible position dans array */
+/* min + 1 = calcul valeur à stocker */
+return (array);
+/* retour pointeur vers début tableau int */
+}
