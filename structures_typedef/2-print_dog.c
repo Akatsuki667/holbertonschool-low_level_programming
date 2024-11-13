11e5401ad036c3434f53f6f75cf2f6aeb9c3e988
Akatsuki667 <qextzy@gmail.com>
2024-11-13 15:01:56 +0100

    add README.md, add file, add C

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - Affiche les informations d'une structure de type dog
+ * @d: pointeur sur la structure dog à afficher
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL) /* vérification paramèttre valide */
+return; /* fin programme */
+if (d->name == NULL)
+printf("Name : (nil)\n");
+else
+printf("Name : %s\n", d->name);
+if (d->age == 0)
+printf("Age : (nil)\n");
+else
+printf("Age : %.2f\n", d->age);
+if (d->owner == NULL)
+printf("Owner : (nil)\n");
+else
+printf("Owner : %s\n", d->owner);
+}
--- structures_typedef/README.md
+++ structures_typedef/README.md
@@ -5,3 +5,7 @@ TASKS :
 dog.h : Define a new type struct dog with the following elements:
 
 1-init_dog.c : Write a function that initialize a variable of type struct dog
+
+2-print_dog.c : Write a function that prints a struct dog
+- If an element of d is NULL, print (nil) instead of this element. (if name is NULL, print Name: (nil))
+- If d is NULL print nothing.
