a09d5abc58e0348653b677976aa56f527f4a21c3
Akatsuki667 <qextzy@gmail.com>
2024-11-13 14:08:04 +0100

    add description

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - structure caractéristique clébard
+ * @name: nom du familier
+ * @age: âge de ce dernier
+ * @owner: le fumier qui le possède
+ */
 struct dog
 {
 char *name;
