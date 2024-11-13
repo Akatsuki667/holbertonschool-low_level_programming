cc13880cba73b01e13a9cb2cf92151383bbc7ad6
Akatsuki667 <qextzy@gmail.com>
2024-11-13 09:44:39 +0100

    change code ehmerce @Jmitchell

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,19 +8,27 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int *array;
+void *array = NULL;
 /* pointeur allocation correspondant type de retour fonction */
 unsigned int i;
+/* index boucle initialisation octet ar*/
+char *ar = NULL;
+/* manipuler la mémoire octet par octet*/
 if (nmemb == 0 || size == 0)
 /* vérification paramètres valide*/
 return (NULL);
 /* fonction retourne NULL */
-array = malloc(nmemb * size); /* allocatin mémoire */
-if (array == NULL) /* vérification échec malloc */
-return (NULL); /* échec malloc */
+array = malloc(nmemb * size);
+/* allocatin mémoire */
+if (array == NULL)
+/* vérification échec malloc */
+return (NULL);
+/* échec malloc */
+ar = array;
+/* initialisation ar vers le premier bytes array*/
 for (i = 0; i < nmemb * size; i++)
-array[i] = 0;
-/* initialisation emplacement mémoire à zéro */
+ar[i] = 0;
+/* initialisation chaque emplacement i mémoire à zéro */
 return (array);
 /* renvoi pointeur pour utilisation bloc mémoire pointée */
 }
