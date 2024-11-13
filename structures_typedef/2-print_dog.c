a5e3913965b094fc23decda6bcd706542bd165c7
Akatsuki667 <qextzy@gmail.com>
2024-11-13 15:05:49 +0100

    add README.md, add file, add C

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,15 +10,15 @@ void print_dog(struct dog *d)
 if (d == NULL) /* vérification paramèttre valide */
 return; /* fin programme */
 if (d->name == NULL)
-printf("Name : (nil)\n");
+printf("Name: (nil)\n");
 else
-printf("Name : %s\n", d->name);
+printf("Name: %s\n", d->name);
 if (d->age == 0)
 printf("(nil)\n");
 else
-printf("Age : %.6f\n", d->age);
+printf("Age: %.6f\n", d->age);
 if (d->owner == NULL)
 printf("(nil)\n");
 else
-printf("Owner : %s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 }
