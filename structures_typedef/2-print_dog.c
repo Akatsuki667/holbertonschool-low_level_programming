a2f47a180c6d49a5af50fe9fbc7cf8c6a08457e8
Akatsuki667 <qextzy@gmail.com>
2024-11-14 15:08:40 +0100

    change code

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,9 +13,6 @@ if (d->name == NULL)
 printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
-if (d->age == 0)
-printf("(nil)\n");
-else
 printf("Age: %.6f\n", d->age);
 if (d->owner == NULL)
 printf("(nil)\n");
