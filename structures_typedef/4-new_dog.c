7df8ae1b8a4cb50e7a05cd75236951e9f72ec767
Akatsuki667 <qextzy@gmail.com>
2024-11-14 15:52:16 +0100

    add README.md, add file, add C

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,56 @@
 #include "dog.h"
 #include <stdlib.h>
-
-dog_t *new_dog(char *name, float age, char *owner)
+/**
+ * _strdup - renvoi pointeur vers allocation dynamique copie string
+ * @str: string à copier
+ * Return: pointeur à renvoyer
+ */
+char *_strdup(char *str)
 {
-dog_t *dog;
-
+char *cp_str;
+int i;
+int len;
+if (str == NULL)
+return (NULL);
+for (len = 0; str[len] != '\0'; len++)
+;
+cp_str = malloc((len + 1) * sizeof(char));
+if (cp_str == NULL)
+return (NULL);
+for (i = 0; i < len; i++)
+cp_str[i] = str[i];
+cp_str[len] = '\0';
+return (cp_str);
+}
+/**
+ * new_dog - Initialise nouvelle variable de type struct dog
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: nom du propriétaire du chien
+ * Return: le gros dogo
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+/* dog_t = type de retour défni par typedef */
+{
+dog_t *dog; /* création structure dog pour dog_t */
+dog = malloc(sizeof(dog_t));
+if (dog == NULL) /* vérification malloc */
+return (NULL); /* si échec malloc retour fonction */
+dog->name = _strdup(name);
+/* copie paramètre nex_dog dans élément struct dog*/
+if (dog->name == NULL) /* vérification élément */
+{
+free(dog); /* libération de mémoire */
+return (NULL); /* échec élément retour fonction*/
+}
+dog->age = age;
+dog->owner = _strdup(owner);
+if (dog->owner == NULL)
+{
+free(dog->name);
+/* si erreur pas besoin zone mémoire pour dog->name */
+free(dog);
+return (NULL);
+}
+return (dog);
 }
