372e73d6f6888ac8767963417411c57055a9b640
Akatsuki667 <qextzy@gmail.com>
2024-11-18 14:51:33 +0100

    add README.md, add file, add code

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - imprimer un nom à l'aide d'un pointeur de fonction
+ * @name: nom à imprimer
+ * @f: fonction qui va imprimer le nom
+ */
+void print_name(char *name, void (*f)(char *))
+/* print_name = délègue impression nom à la fonction pointé par f*/
+/* name = pointeur vers chaîne de carctère (nom à imprimer) */
+/* f = pointeur de fonction, parmaètre char, type de retour void */
+{
+if (name == NULL || f == NULL)
+/* condition vérification validité paramètres */
+return;
+/* Si invalide retour de fonction */
+f(name);
+/* Appel focntion pour print l'argument donné */
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,5 @@
+Do the task of the project "C-Function pointer"
+
+TASKS :
+
+0-print_name.c : Write a function that prints a name.
