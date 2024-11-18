c021516d133b63ba4da3dd486b1342cde0575be5
Akatsuki667 <qextzy@gmail.com>
2024-11-18 16:19:08 +0100

    add README.md, add file, add code

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - recherhe un nb entier
+ * @array: pointeur vers tableau d'entiers
+ * @size: nb d'éléments dans array
+ * @cmp: poiteur vers fonction qui s'appliquera à chaque élément
+ * Return: -1 si aucune correspondance
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+/* index parcourir array */
+if (size <= 0)
+/* condition vérification validité paramètre */
+return (-1);
+/* si array vide retour valeur d'erreur */
+for (i = 0; i < size - 1; i++)
+/* parcourir indices array */
+{
+if (cmp(array[i]) != 0)
+/* application fonction cmp sur chaque élément */
+return (i);
+/* retourne indice élément de comparaison*/
+}
+return (-1);
+/* valeur de retour -> aucune correspondance trouvé */
+}
--- function_pointers/README.md
+++ function_pointers/README.md
@@ -5,3 +5,8 @@ TASKS :
 0-print_name.c : Write a function that prints a name.
 
 1-array_iterator.c : Write a function that executes a function given as a parameter on each element of an array.
+
+2-int_index.c : Write a function that searches for an integer.
+- int_index returns the index of the first element for which the cmp function does not return 0
+- If no element matches, return -1
+- If size <= 0, return -1
