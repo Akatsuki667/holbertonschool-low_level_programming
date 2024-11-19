8d2cdf820f1b04ad3b5e9ffdb049b7d36aca456b
Akatsuki667 <qextzy@gmail.com>
2024-11-19 06:01:02 +0100

    add README.md, add code

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,10 @@
+#include "calc.h"
+/**
+ * get_op_func - Sélectionne la fonction correcte pour l'opération.
+ * @s: Le symbole de l'opération (ex: "+", "-").
+ * Return: Un pointeur vers la fonction correspondante, ou NULL si non trouvé.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include 'calc.h'
+/**
+ * op_add - additionner paramètres
+ * @a: valeur à additionner
+ * @b: valeur à additionner
+ * Return: addition des valeurs
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - soustraction paramètres
+ * @a: valeur à soustraire
+ * @b: valeur à soustraire
+ * Return: soustraction des valeurs
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - multiplication paramètres
+ * @a: valeur à multiplier
+ * @b: valeur à multiplier
+ * Return: multiplication des valeurs
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - diviser paramètres
+ * @a: valeur à diviser
+ * @b: valeur à diviser
+ * Return: diviser des valeurs
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - reste euclidien paramètres
+ * @a: valeur à modulo
+ * @b: valeur à modulo
+ * Return: reste euclidien des valeurs
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
