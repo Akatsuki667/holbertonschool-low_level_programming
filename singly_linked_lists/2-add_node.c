3514a041b0af831aaa96c50d2837e285ec076813
Akatsuki667 <qextzy@gmail.com>
2024-12-03 11:16:43 +0100

    add _strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 /**
+ * _strlen - Calcule la longueur d'une chaîne de caractères
+ * @s: Pointeur vers la chaîne de caractères
+ * Return: La longueur de la chaîne
+ */
+int _strlen(const char *s)
+{
+int len = 0;
+while (*s != '\0')
+/* Boucle jusqu'à la fin de la chaîne */
+/* != : différent de */
+{
+len++;
+/* Incrémente la longueur */
+s++;
+/* Passe au caractère suivant */
+}
+return (len);
+/* Retourne la longueur totale */
+}
+/**
  * add_node - ajout nouveau noeuds au début de la liste chaînée
  * @head: double pointeur vers noeud("tête") liste chaînée
  * @str: pointeur vers chaîne de caractère que le noeud doit stocker
@@ -15,6 +35,7 @@ return (NULL);
 new->str = strdup(str);
 /* allocation mémoire copie str */
 /* retour pointeur copie */
+new->len = _strlen(str);
 new->next = *head;
 /* champ du nouveau noeud qui pointe vers le premier noeud actuel */
 /* *head->pointe vers tête actuelle de la liste */
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,31 @@
 #include "lists.h"
 /**
+ * _strlen - Calcule la longueur d'une chaîne de caractères
+ * @s: Pointeur vers la chaîne de caractères
+ * Return: La longueur de la chaîne
+ */
+int _strlen(const char *s)
+{
+int len = 0;
+while (*s != '\0')
+/* Boucle jusqu'à la fin de la chaîne */
+/* != : différent de */
+{
+len++;
+/* Incrémente la longueur */
+s++;
+/* Passe au caractère suivant */
+}
+return (len);
+/* Retourne la longueur totale */
+}
+/**
+ * add_node - ajout nouveau noeuds au début de la liste chaînée
+ * @head: double pointeur vers noeud("tête") liste chaînée
+ * @str: pointeur vers chaîne de caractère que le noeud doit stocker
+ * Return: adresse nouvelle élément ou NULL
+ */
+/**
  * add_node_end - création noeud
  * @head: poin teur vers pointeur du premier noeud
  * @str: chaîne de caractère stocker dans le nouveau noeud
@@ -14,6 +40,7 @@ new = malloc(sizeof(list_t)); /* allocation mémoire */
 if (new == NULL) /* vérification allocation */
 return (NULL);
 new->str = strdup(str); /* allocation mémoire copie str */
+new->len = _strlen(str);
 new->next = NULL; /* initialisation champ nouveau noeud */
 if (*head == NULL) /* vérification liste valide */
 {
