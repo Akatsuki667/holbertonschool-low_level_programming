ee0f3b43c659e09394eff16c4f2cf6d313be44a7
Akatsuki667 <qextzy@gmail.com>
2024-12-03 14:57:12 +0100

    add file, add README.md, add code

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * dlistint_len - calculer nb noeuds
+ * @h: pointeur adressse mémoire premier noeud
+ * Return: nb de noeuds
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 const dlistint_t *current = h;
---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_dnodeint - ajouter un noeud
+ * @head: double pointeur vers adresse premier noeud
+ * @n: données du nouveau noeud
+ * Return: adresse nouvel élément ou NULL
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *temp;
+/* stockage adresse allocation mémoire */
+dlistint_t *ptr;
+/* stockage adresse mémoire head */
+/* ne pas perdre adresse mémoire initial */
+ptr = *head; /* assignation adresse mémoire */
+temp = malloc(sizeof(dlistint_t));
+/* allocation mémoire noeuds */
+if (temp == NULL) /* vérification allocation valide */
+return (NULL);
+if (ptr != NULL) /* vérification liste non vide */
+ptr->prev = temp; /* assignation adresse mémoire */
+temp->n = n; /* assignation champ n paramètre n */
+temp->prev = NULL;
+/* assignation pointeur car premiere élément */
+temp->next = *head;
+/* assignation pointeur suivant vers adresse mémoire head */
+/* le premier élément devient le second */
+*head = temp;
+/* assignation adresse mémoire premier élément à head */
+return (temp);
+}
