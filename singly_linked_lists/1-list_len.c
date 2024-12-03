49c9357981825e0845d0154cead826fcce6be711
Akatsuki667 <qextzy@gmail.com>
2024-12-03 10:31:49 +0100

    change code

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,8 +8,6 @@ size_t list_len(const list_t *h)
 {
 const list_t *current = h; /* pointeur permettant l'itération */
 size_t count = 0; /* stockage nb de noeuds liste */
-if (current == NULL) /* vérification paramètres valide */
-return (-1);
 while (current != NULL) /* parcours paramètres */
 {
 current = current->next; /* MAJ pointeur vers prochaion noeud */
