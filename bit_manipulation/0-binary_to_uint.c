0394e4b63b0dc5c017cc1beead07308f3781a61a
Akatsuki667 <qextzy@gmail.com>
2024-12-04 17:57:38 +0100

    add commentary

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -6,15 +6,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int n = 0;
-int i;
-if (b == NULL)
+unsigned int n = 0; /* stockage conversion nb binaire */
+int i; /* index parcourir b */
+if (b == NULL) /* vérification paramètre valide */
 return (0);
 for (i = 0; b[i] != '\0'; i++)
 {
 if (b[i] != '0' && b[i] != '1')
+/* vérification chaque caractère de b soit 0 ou 1 */
 return (0);
-n = (n << 1) | (b[i] - '0');
+n = (n << 1) | (b[i] - '0'); /* conversion binaire vers entier */
+/* n<<1 = décalage gauche position bit, revient à multiplier n par 2 */
+/* | -> ajout nouveau bit fin de n */
+/* b[i] - '0' -> conversions caractère binaires */
 }
 return (n);
 }
