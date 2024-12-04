b4dc690d4b565abf4512a8247e0a6e715fcd0353
Akatsuki667 <qextzy@gmail.com>
2024-12-04 09:46:57 +0100

    change code

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -6,14 +6,13 @@
 void free_dlistint(dlistint_t *head)
 {
 dlistint_t *next_node; /* stocker l'adresse du noeud suivant */
-dlistint_t *current = head;
-/* pointeur vers struct */
-/*-> permet itération pour ne pas perdre valeur de head */
 if (head == NULL) /* vérification paramètre valide */
 return;
-while (current != NULL) /* parcours liste de noeud */
-next_node = current->next; /* assignation adresse noeud suivant */
-free(current); /* libération du noeud */
-current = next_node;
+while (head != NULL) /* parcours liste de noeud */
+{
+next_node = head->next; /* assignation adresse noeud suivant */
+free(head); /* libération du noeud */
+head = next_node;
 /* passer au noeud suivant (incrémentation séquentielle) */
+}
 }
