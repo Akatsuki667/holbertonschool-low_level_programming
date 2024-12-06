2df86ace3f701098817d1348a1a586bf5f26ce4c
Akatsuki667 <qextzy@gmail.com>
2024-12-06 11:23:33 +0100

    change code

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index > 63) /* vérification index valide */
 return (-1);
-*n = *n & (1UL << index); /* bit position index forcé à 0 */
+*n = *n & ~(1UL << index); /* bit position index forcé à 0 */
 /* (~(1UL << index)) -> création masque avec bit ciblé à 1 */
 /* (~(1UL << index)) inverse tous les bits */
 return (1);
